529390fee439cfefba95267dfad3176b1405216c
ingiman1 <ingiman1@gmail.com>
2020-02-05 19:33:59 -0800

    Revert "changed auto path"
    This reverts commit 7e26891f26e22d9af2e076be248ebc8af6ff1d10.

--- new1point/src/main.cpp
+++ new1point/src/main.cpp
@@ -38,7 +38,7 @@ motor ArmR(PORT3, gearSetting::ratio36_1, true);
 motor ArmL(PORT7, gearSetting::ratio36_1, false);
 
 motor IntakeOne(PORT13, gearSetting::ratio18_1, true); // right
-motor IntakeTwo(PORT14, gearSetting::ratio18_1, false);
+motor IntakeTwo(PORT17, gearSetting::ratio18_1, false);
 
 // sonar RulerL = sonar(Brain.ThreeWirePort.A); this will break the code, gives
 // memory permission error
@@ -313,19 +313,19 @@ void autonomous(void) {
   IntakeOne.stop();
   IntakeTwo.stop();
 
-  ArmL.startSpinFor(directionType::rev, 0.87, rotationUnits::rev, 80, velocityUnits::pct);
-  ArmR.startSpinFor(directionType::rev, 0.87, rotationUnits::rev, 80, velocityUnits::pct);
+  ArmL.startSpinFor(directionType::rev, 0.7, rotationUnits::rev, 80, velocityUnits::pct);
+  ArmR.startSpinFor(directionType::rev, 0.7, rotationUnits::rev, 80, velocityUnits::pct);
 
   task::sleep(100);
 
-  NewPID(0, 65, 2.8, -0.6, 4.5, 58);
-  NewPID(94, 77, 2.4, 0, 1, 40);
+  NewPID(0, 65, 2.8, -0.6, 4, 58);
+  NewPID(92, 77.5, 2.4, 0, 1, 40);
 
   IntakeOne.spin(directionType::rev, 100, velocityUnits::pct);
   IntakeTwo.spin(directionType::rev, 100, velocityUnits::pct);
 
-  ArmL.startSpinFor(directionType::fwd, 0.92, rotationUnits::rev, 15, velocityUnits::pct);
-  ArmR.spinFor(directionType::fwd, 0.92, rotationUnits::rev, 15, velocityUnits::pct);
+  ArmL.startSpinFor(directionType::fwd, 0.75, rotationUnits::rev, 15, velocityUnits::pct);
+  ArmR.spinFor(directionType::fwd, 0.75, rotationUnits::rev, 15, velocityUnits::pct);
 
   task::sleep(150);
 
@@ -342,24 +342,24 @@ void autonomous(void) {
 
   task::sleep(350);
 
-  FRDrive.spinFor(6.8, rotationUnits::rev, 100, velocityUnits::pct, false);
+  FRDrive.spinFor(8, rotationUnits::rev, 100, velocityUnits::pct, false);
   BRDrive.spinFor(-3, rotationUnits::rev, 50, velocityUnits::pct, false);
   FLDrive.spinFor(-3, rotationUnits::rev, 50, velocityUnits::pct, false);
-  BLDrive.spinFor(6.8, rotationUnits::rev, 100, velocityUnits::pct, true);
+  BLDrive.spinFor(8, rotationUnits::rev, 100, velocityUnits::pct, true);
 
   task::sleep(100);
 
-  FRDrive.spinFor(2, rotationUnits::rev, 90, velocityUnits::pct, false);
-  BRDrive.spinFor(2, rotationUnits::rev, 90, velocityUnits::pct, false);
-  FLDrive.spinFor(2, rotationUnits::rev, 90, velocityUnits::pct, false);
-  BLDrive.spinFor(2, rotationUnits::rev, 90, velocityUnits::pct, true);
+  FRDrive.spinFor(2.7, rotationUnits::rev, 100, velocityUnits::pct, false);
+  BRDrive.spinFor(2.7, rotationUnits::rev, 100, velocityUnits::pct, false);
+  FLDrive.spinFor(2.7, rotationUnits::rev, 100, velocityUnits::pct, false);
+  BLDrive.spinFor(2.7, rotationUnits::rev, 100, velocityUnits::pct, true);
 
-  task::sleep(400);
+  task::sleep(200);
  
   IntakeOne.spin(directionType::fwd, 100, velocityUnits::pct);
   IntakeTwo.spin(directionType::fwd, 100, velocityUnits::pct);
 
-  task::sleep(300);
+  task::sleep(200);
 
   ArmL.startSpinFor(directionType::rev, 1, rotationUnits::rev, 30, velocityUnits::pct);
   ArmR.spinFor(directionType::rev, 1, rotationUnits::rev, 30, velocityUnits::pct);
